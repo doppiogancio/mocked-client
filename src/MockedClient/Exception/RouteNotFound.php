761e43ce4b5eba2f60fb88aec941d60856111f35
Fabrizio Gargiulo <fabrizio.gargiulo@sourceability.com>
2022-07-21 07:45:14 +0200

    Routes added to RouteNotFoundException

--- src/MockedClient/Exception/RouteNotFound.php
+++ src/MockedClient/Exception/RouteNotFound.php
@@ -4,20 +4,49 @@ declare(strict_types=1);
 
 namespace DoppioGancio\MockedClient\Exception;
 
+use DoppioGancio\MockedClient\Route\Route;
 use Exception;
 
 use function sprintf;
 
 class RouteNotFound extends Exception
 {
-    public function __construct(string $method, string $path)
+    /** @var Route[] */
+    private array $routes;
+
+    /**
+     * @param Route[] $routes
+     */
+    public function __construct(string $method, string $path, array $routes)
     {
         $message = sprintf(
-            'Mocked route %s %s not found',
+            "Mocked route %s %s not found \n%s",
             $method,
-            $path
+            $path,
+            $this->routesToString($routes)
         );
 
+        $this->routes = $routes;
+
         parent::__construct($message);
+    }
+
+    /**
+     * @param Route[] $routes
+     */
+    private function routesToString(array $routes): string
+    {
+        $string = "Mocked routes:\n\n";
+
+        foreach ($routes as $route) {
+            $string .= sprintf("ROUTE %s %s\n", $route->getMethod(), $route->getPath());
+        }
+
+        return $string;
+    }
+
+    public function getRoutesAsString(): string
+    {
+        return $this->routesToString($this->routes);
     }
 }
