3a41a26c131eed1555bb501e330e287357e75c29
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2022-07-21 14:45:23 +0200

    Rolling back RouteNotFound routes (#15)
    Co-authored-by: Fabrizio Gargiulo <fabrizio.gargiulo@sourceability.com>

--- src/Exception/RouteNotFound.php
+++ src/Exception/RouteNotFound.php
@@ -4,48 +4,20 @@ declare(strict_types=1);
 
 namespace DoppioGancio\MockedClient\Exception;
 
-use DoppioGancio\MockedClient\Route;
 use Exception;
 
 use function sprintf;
 
 class RouteNotFound extends Exception
 {
-    /** @var Route[] */
-    private array $routes;
-
-    /**
-     * @param Route[] $routes
-     */
-    public function __construct(string $method, string $path, array $routes = [])
+    public function __construct(string $method, string $path)
     {
-        $this->routes = $routes;
-        $message      = sprintf(
-            "Mocked route %s %s not found\n\n%s",
+        $message = sprintf(
+            'Mocked route %s %s not found',
             $method,
-            $path,
-            $this->routesToString($routes)
+            $path
         );
 
         parent::__construct($message);
-    }
-
-    /**
-     * @param Route[] $routes
-     */
-    private function routesToString(array $routes): string
-    {
-        $string = "Mocked routes:\n\n";
-
-        foreach ($routes as $route) {
-            $string .= sprintf("ROUTE %s %s\n", $route->getMethod(), $route->getPath());
-        }
-
-        return $string;
-    }
-
-    public function getRoutesAsString(): string
-    {
-        return $this->routesToString($this->routes);
     }
 }
